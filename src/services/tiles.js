82c71eb3f82f84f54ce04b8356539418d639693b
David Duffy <daviduffy1@gmail.com>
2020-12-29 13:45:06 -0800

    got low-left-to-high-right diagonal returning correct array of values

--- src/services/tiles.js
+++ src/services/tiles.js
@@ -18,6 +18,7 @@ export const getArrays = ({ index, sideLength, tiles }) => {
   const horizontal = [...tiles].slice(rowStart, rowStart + sideLength);
   const vertical = tiles.filter(({ index: i }) => i % sideLength === colIndex);
   let hltolr = null;
+  let lltohr = null;
   {
     // horizontal high left to low right
     const plus1 = sideLength + 1;
@@ -30,26 +31,50 @@ export const getArrays = ({ index, sideLength, tiles }) => {
       location = 'highSide';
       length = (rowIndex - 1) + (sideLength - (colIndex - 1));
     } else {
-      location = 'lowside';
+      location = 'lowSide';
       length = sideLength - (rowIndex - 1) + (colIndex - 1);
     }
     const start = location === 'highSide' ? (colIndex - rowIndex) : (rowIndex - colIndex) * 10;
-    console.log({ rowIndex, rowStart, colIndex, start, length });
     // eslint-disable-next-line
     const indeces = new Array(length).fill(null).map((val, index) => ((index * plus1) + start));
     hltolr = indeces.map((val) => tiles[val]);
   }
-  return ({ horizontal, vertical, hltolr });
+  {
+    // horizontal low left to high right
+    lltohr = [];
+    const minus1 = sideLength - 1;
+    let length;
+    let location;
+    if (colIndex + rowIndex === minus1) {
+      location = 'centerLine';
+      length = 11;
+    } else if (colIndex + rowIndex < minus1) {
+      location = 'highSide';
+      length = colIndex + rowIndex + 1;
+    } else {
+      location = 'lowSide';
+      length = (minus1 - colIndex) + 1 + (minus1 - rowIndex);
+    }
+    const lastRowFirstIndex = (sideLength * sideLength) - sideLength;
+    const start = location !== 'lowSide'
+      ? ((colIndex + rowIndex) * 10)
+      : lastRowFirstIndex + ((rowIndex + colIndex) % 10) + 1;
+    // eslint-disable-next-line
+    const indeces = new Array(length).fill(null).map((val, index) => (start - (index * minus1)));
+    lltohr = indeces.map((val) => tiles[val]);
+  }
+  return ({ horizontal, vertical, hltolr, lltohr });
 };
 
 export const getNextTiles = ({ state, index }) => {
   const { currentPlayer, sideLength: prevSideLength, tiles: prevTiles } = state;
-  const neighbors = getNeighborIndexes({ index, sideLength: prevSideLength });
+  // const neighbors = getNeighborIndexes({ index, sideLength: prevSideLength });
   const arrays = getArrays({ index, sideLength: prevSideLength, tiles: prevTiles });
-  console.log({
-    neighbors: JSON.parse(JSON.stringify(neighbors)),
-    arrays: JSON.parse(JSON.stringify(arrays)),
-  });
+  console.log(JSON.parse(JSON.stringify(arrays)));
+  // console.log({
+  //   neighbors: JSON.parse(JSON.stringify(neighbors)),
+  //   arrays: JSON.parse(JSON.stringify(arrays)),
+  // });
   const nextTiles = prevTiles.map(({ index: i, owner: prevOwner }) => {
     const owner = i === index ? currentPlayer : prevOwner;
     return { index: i, owner };
